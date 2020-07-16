3e9e921db5737b91d713126ace1e6eaccb50f2f4
oleg <perchykoleg@gmail.com>
2020-07-16 08:36:23 -0500

    EP29-30 Foreign Keys and DB Factories
    In these modules I set up an Article some Eloquent relations in the
    Aarticles model.
    Created a foreign key user_id in the articles  table so that
    orphaned articles are deleted when a user is deleted.
    Additionally created an Article Factory which may be used to quickly
    generate random articles in php Tinker using the Faker library.

--- app/Article.php
+++ app/Article.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
@@ -11,5 +12,17 @@ class Article extends Model
    public function path()
    {
       return route('articles.show', $this);
+   }
+
+   public function user()
+   {
+      return $this->belongsTo(User::class);
+   }
+
+   public function author()
+   {
+      return $this->belongsTo(User::class, 'user_id'); 
+      // in this case we need to pass the foreign key as an argument because Laravel 
+      // expects a certain convention to be followed
    }
 }

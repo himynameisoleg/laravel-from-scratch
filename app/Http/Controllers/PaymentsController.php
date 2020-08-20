ccd901d2a8b7218eab37561acaf86de02d0cbc93
oleg <perchykoleg@gmail.com>
2020-08-20 13:19:59 -0500

    EP49 Events
    Sometimes we want to seperate out the core logic of the Controller and
    its sideeffects. Eg after a user purchases a product we want to
    send an email confirmations, award them an achivement, and queue up an
    email to be sent with a discount code in the future.
    
    While this can be done procedurally in the Controller this may end up
    being very long. There is where Events come into play.
    
    Created a ProductPurchased event using:
    `php artisan make:event ProductPurchased`
    Some uncessary code was stripped from this class.
    Now we can dispatch this event in 2 different ways, most commonly with
    the 'event()' method.
    Next we created several listeners:
    `php artisan make:listener AwardAchievement -e ProductPurchased`
    The `-e` flag simply points our listener to the ProductPurchased class.
    Lastly we needed to hook each listener with with the new event.
    This can either be done verbose in the the EventServiceProvider.php
    listen array. Or we can have laravel do some of its magic and override
    the 'shouldDiscoverEvents()' method to return true.
    Now each listener will be paired with the corresponding event.

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ProductPurchased;
 use App\Notifications\PaymentReceived;
 use Illuminate\Http\Request;
+use Illuminate\Queue\ListenerOptions;
 use Illuminate\Support\Facades\Notification;
 
 class PaymentsController extends Controller
@@ -15,11 +17,31 @@ class PaymentsController extends Controller
 
     public function store()
     {
+        
         // Notification::send(request()->user(), new PaymentReceived());
-
         // more commonly we use this:
-        request()->user()->notify(new PaymentReceived(900));
+        // request()->user()->notify(new PaymentReceived(900));
 
-        return redirect()->route('home');
+
+        // Eventing on product purchase
+        // 1. process payment 
+        // 2. unlock purchase 
+
+        // Side Effects / Listener
+        // 1. notify user about purchase
+        // 2. award achievenet (badge or something)
+        // 3. send sharable coupon to user at some later time
+
+
+        // Lets say a user purchased a toy on your site, we can dispatch an event ProductPurchase
+        // ProductPurchased::dispatch('toy');
+        // OR (effectively doing the same) 
+        event(new ProductPurchased('toy'));
+        // then we create a listener called AwardAchievements and wire it in the EventServiceProvider.php
+
+        // run `php artisan event:list` to see what events have which listeners
+
+
+        // return redirect()->route('home');
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\ProductPurchased;
+use App\Listeners\AwardAchievements;
+use App\Listeners\SendShareableCoupon;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +21,12 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        // This is the verbose way of hooking up Events to Listeners, however
+        // we can override a Laravel method here called 'shouldDiscoverEvents()'
+        // ProductPurchased::class => [
+        //     AwardAchievements::class,
+        //     SendShareableCoupon::class
+        // ]
     ];
 
     /**
@@ -30,5 +39,9 @@ class EventServiceProvider extends ServiceProvider
         parent::boot();
 
         //
+    }
+    public function shouldDiscoverEvents()
+    {
+        return true;
     }
 }

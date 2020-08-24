ee5ed45f0fddf90743d163c72ddbffad0a742c01
oleg <perchykoleg@gmail.com>
2020-08-24 10:42:23 -0500

    EP52 slightly shorthand way to authorize

--- app/Http/Controllers/ConversationBestReplyController.php
+++ app/Http/Controllers/ConversationBestReplyController.php
@@ -12,6 +12,11 @@ class ConversationBestReplyController extends Controller
         // authorize that the current user has  permission to set the best reply for the conversation
         $this->authorize('update', $reply->conversation);
 
+        // optionally we can remove the explicit 'update' argument and Laravel will check the controller 
+        // method name. If it matched the laravel conventions then it will assume the user is authorized
+        // Generally, you probbaly want to be explicit.
+        // $this->authorize($reply->conversation);
+
         // then set it
         $reply->conversation->setBestReply($reply);
 
